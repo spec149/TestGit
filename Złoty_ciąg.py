c16b34889c1d965f4f7514843d479961617ca7bd
spec149 <spec149@gmail.com>
2020-12-07 13:44:50 +0100

    Add files via upload

---
+++ Złoty_ciąg.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+j = 0
+for i in range(1, 11):
+    j = i+j
+    print(j)
+
+for i in range(1, 11):
+    print('Wartość liczby', i, 'podniesionej do sześcianu wynosi:', i**3)
