d2e06988522055376ca028cac7c496353650e1dd
spec149 <spec149@gmail.com>
2020-12-07 13:24:06 +0100

    Update silnia.py

--- silnia.py
+++ silnia.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-num = int(input('Podaj dowolną liczbę całkowitą do 310: '))
+num = int(input('Podaj dowolną liczbę całkowitą do 30: '))
 j, i = 1, 1
 while i != num:
     for i in range(1, num):
